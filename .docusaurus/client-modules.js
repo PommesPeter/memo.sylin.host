a6aad26c60affacc6500954e198e9d15a97ef367
PommesPeter <434596665@qq.com>
2022-02-09 20:21:04 +0800

    remove all files to restart

--- .docusaurus/client-modules.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\src\\css\\custom.css"),
-  require("E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\remark-admonitions\\styles\\infima.css"),
-];
