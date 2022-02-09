3f6c2f739b604556ae2b80ba88e3618ef5918055
PommesPeter <434596665@qq.com>
2022-02-09 20:37:07 +0800

    init: reinit repo

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('E:\\workspace\\MemoLibrary\\memo.pommespeter.space\\src\\css\\custom.css'),
+];
