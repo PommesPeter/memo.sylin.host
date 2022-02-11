320e041af9e403cbb3ab5949cbc5f32ba1ccd45c
PommesPeter <434596665@qq.com>
2022-02-11 21:17:19 +0800

    feat: updated code style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/okaidia');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 const math = require('remark-math');
 const katex = require('rehype-katex');
