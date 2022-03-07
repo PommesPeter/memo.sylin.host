14fb2d4aadb57007cd7af61f5b2c3b974c61d4e9
PommesPeter <434596665@qq.com>
2022-03-07 15:15:29 +0800

    added leetcode note and update homepage

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -8,8 +8,9 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Enjoy every day in your life. Life is not only about learning, but
+        more beautiful things are waiting for you. Make life better through
+        the little details in life.
       </>
     ),
   },
@@ -18,8 +19,8 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Turn learning into a skill and make the most of your knowledge
+        to make life better. Focus on creating better things.
       </>
     ),
   },
@@ -28,8 +29,8 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Create something interesting through code, improve the efficiency
+        and way of daily work, and innovate to make life better.
       </>
     ),
   },
