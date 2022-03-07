7a3ab5f5438fb5d1475ce798fefcab03c37b4c4c
PommesPeter <434596665@qq.com>
2022-03-07 21:57:51 +0800

    updated links pages

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
 import arrayShuffle from "array-shuffle";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 
 function About() {
   return (
@@ -14,6 +16,7 @@ function About() {
 
 interface FriendData {
   pic: string;
+  gitName: string;
   name: string;
   intro: string;
   url: string;
@@ -27,39 +30,61 @@ function githubPic(name: string) {
 var friendsData: FriendData[] = [
   {
     pic: githubPic("lideming"),
+    gitName: "lideming",
     name: "lideming",
     intro: "Building random things with Deno, Node and .NET Core.",
     url: "https://yuuza.net/",
-    note: "我的大学同学，某些时候是同事，也有时是队友。全栈/APEX/R6/CSGO/BFV，有些懒。",
+    note: "工具箱的工具，能够解决99.999%的系统问题，似乎他帮我装好了ArchLinux，解决了无数的bug。",
   },
   {
     pic: githubPic("Therainisme"),
     name: "Therainisme",
+    gitName: "Therainisme",
     intro: "寄忆犹新",
     url: "https://notebook.therainisme.com/",
-    note: "雨神，一出门就下雨。我大学期间的同事，任我社2019级多媒体部部长。网站开发运维专家。",
+    note: "雨神，一出门就下雨。我大学期间的同事，任我社2019级多媒体部部长。前端小能手，我们经常一起捣鼓奇怪的小工具。",
   },
   {
     pic: githubPic("AndSonder"),
     name: "Sonder",
+    gitName: "AndSonder",
     intro: "life is but a span, I use python",
     url: "https://blog.keter.top/",
-    note: "科协F4的成员。我大学期间的同事，任我社2019级软件部部长。主攻方向是神经网络攻击。他找女朋友之前我们常一起吃饭。",
+    note: "科协F4的成员。我大学期间的同事，任我社2019级软件部部长。主攻方向是神经网络攻击。",
   },
   {
     pic: githubPic("Zerorains"),
     name: "Zerorains",
+    gitName: "Zerorains",
     intro: "life is but a span, I use python",
     url: "https://blog.zerorains.top",
-    note: "科协F4的成员。科协恶霸(x)。主攻方向是基于深度学习技术图像分割。他不太喜欢看我抽卡。",
+    note: "科协F4的成员。主攻方向是基于深度学习技术图像分割。",
   },
   {
     pic: githubPic("breezeshane"),
     name: "Breeze Shane",
+    gitName: "breezeshane",
     intro: "一个专注理论但学不懂学不会的锈钢废物，但是他很擅长产出Bug，可能是因为他体表有源石结晶分布，但也可能仅仅是因为他是Bug本体。",
     url: "https://breezeshane.github.io/",
-    note: "一代传奇，手撸GAN的老单。",
+    note: "试图爆肝原理，理解GAN的传奇。",
+  },
+  {
+    pic: githubPic("VisualDust"),
+    name: "VisualDust",
+    gitName: "VisualDust",
+    intro: "Rubbish CVer | Poor LaTex speaker | Half stack developer | 键圈躺尸砖家",
+    url: "https://focus.visualdust.space/",
+    note: "家喻户晓的工具箱，好像能解决99.9%的问题。我们喜欢找他和他的工具解决问题。",
+  },
+  {
+    pic: githubPic("Fly-Pluche"),
+    name: "Fly-Pluche",
+    gitName: "Fly-Pluche",
+    intro: "The man is too lazy to introduce himself",
+    url: "https://fly-pluche.github.io/",
+    note: "善良活泼的小伙子，喜欢问问题，喜欢思考问题，对事情很积极上心。",
   }
+
 ];
 
 function Friends() {
@@ -127,7 +152,14 @@ function FriendCard(props: { data: FriendData; fadeout?: boolean }) {
           />
         </div>
         <div className="card__body">
-          <h2>{data.name}</h2>
+          <h2>
+            {data.name}
+            <a href={"https://github.com/" + data.gitName}>
+            <FontAwesomeIcon className="user-profile-icon" icon={faGithubAlt} />
+            </a>
+
+          </h2>
+          
           <p>
             <big>{data.intro}</big>
           </p>
